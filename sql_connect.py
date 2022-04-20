dbabb8882b2a69a2ef0c64f08fef4584d42c2d44
keholsa <99051518+keholsa@users.noreply.github.com>
2022-04-19 21:06:30 -0500

    Delete sql_connect.py

--- sql_connect.py
+++
@@ -1,9 +0,0 @@
-import pyodbc 
-
-server = '10.0.0.4\TEST44\SQLEXPRESS, 1433' 
-database = 'master' 
-username = 'sa' 
-password = '2Tfu*(eZj0$tNbO' 
-
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
