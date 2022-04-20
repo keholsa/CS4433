83516d0d4b49bfc8c9e93d32c0dd26c1987b50ba
GipsonL <78498165+GipsonL@users.noreply.github.com>
2022-04-20 01:57:43 -0500

    Add files via upload

--- Database1.py
+++ Database1.py
@@ -2,12 +2,11 @@
 #  Pip install sqlalchemy
 #  Pip install sqlalchemy.orm
 from sqlalchemy import *
-from sqlalchemy.orm import Session, sessionmaker
-
+from sqlalchemy.orm import Session, sessionmaker, scoped_session
+from markupsafe import escape # Standard library
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-metadata = MetaData()
-
+session = sessionmaker(bind=engine)
 
 #schedule table
 with engine.connect() as conn:
@@ -25,15 +24,15 @@ Sunday_schedule varchar(40)
 """))
     #Fill table
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','NA','2:00 PM, 3:00 PM','NA','10:00 PM, 11:00 PM', 'NA')"""))
+VALUES (1,'NA','NA','14:00, 15:00','NA','10:00, 11:00', 'NA')"""))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','NA','2:00 PM, 3:00 PM','NA','10:00 PM', '12:00 PM, 1:00 PM, 3:00 PM')""" ))
+VALUES (1,'NA','NA','12:00, 15:00','NA','10:00', '12:00, 13:00, 15:00')""" ))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'4:00 PM, 5:00 PM','NA','NA','NA','12:30 PM, 1:30 PM', 'NA')""" ))
+VALUES (1,'16:00, 17:00','NA','NA','NA','12:30, 13:30 PM', 'NA')""" ))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'10:00 AM, 4:00 PM','10:00 AM, 3:00 PM','10:00 AM, 4:00 PM','10:00 AM, 3:00 PM','10:00 PM', '12:00 PM, 1:00 PM, 3:00 PM')""" ))
+VALUES (1,'10:00, 16:00 PM','10:00 AM, 3:00 PM','10:00 AM, 4:00 PM','10:00 AM, 3:00 PM','10:00 PM', '12:00 PM, 1:00 PM, 3:00 PM')""" ))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'4:00 PM, 6:00 PM, 7:00 PM','NA','NA','NA','NA', '3:00 PM, 4:00 PM, 5:00 PM, 6:00 PM, 7:00 PM') """ ))
+VALUES (1,'16:00, 18:00, 20:00','NA','NA','NA','NA', '15:00, 12:00, 5:00 PM, 6:00 PM, 7:00 PM') """ ))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
 VALUES (1,'12:30 PM, 1:30 PM, 2:30 PM, 3:30 PM, 4:30 PM, 5:30 PM','NA','12:30 PM, 1:30 PM, 2:30 PM, 3:30 PM, 4:30 PM, 5:30 PM','NA','NA', 'NA')"""))
     conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
@@ -78,7 +77,9 @@ name varchar(32),
 Foreign Key (Schedule_Id) references W_SCHEDULE(schedule_id)
 )
 """ ))
-    #No prefill. Filled at runtime.
+    #For testing
+    conn.execute(text("INSERT INTO Tutee VALUES ('A5879321', 4, 'TestBoi')"))
+    #No other prefill. Filled at runtime.
     
 
 #Class table
@@ -187,35 +188,132 @@ Class_id varchar(8)
 
     conn.execute(text("""
 CREATE TABLE APPOINTMENT(
-Appointment_ID Integer auto_increment PRIMARY KEY,
-a_date DATE,
-a_time TIME,
+Appointment_ID Integer PRIMARY KEY,
+a_date varchar(16),
+a_time varchar(16),
 Tutor_id varchar(16),
 Tutee_id varchar(16),
-uOnline Integer,
 Class_id varchar(8),
 Foreign Key (Class_id) references CLASS(Class_id),
 Foreign Key (tutor_id) references Tutor(Cwid), 
 Foreign Key (Tutee_id) references Tutee(Cwid)
 )
 """))
-    #No prefill. Filled at runtime.
+    
+
+
+    conn.commit()
+    
      
-    print()
+### Here on out is simply testing code, not database code. 
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = get_Tutor("'Mitchel Murray'")
+    return f"{escape(name)}"
+
+@app.route("/SignUp")
+def sign_up():
+    #Oh yeah. I'm that guy. On server html. 
+    return ("""<form action="action_page.php" style="border:1px solid #ccc">
+  <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>cwid</b></label>
+    <input type="text" placeholder="Enter cwid" name="cwid" required>
     
-    print("RESULTS LOOKIE HERE VVVVVVVVVV")
-    result = conn.execute(text("SELECT name from CLASS"))
-    for row in result:
-        print(f"{row.name}")
-    print("RESULTS LOOKIE HERE" )
-    print()
+    <label for="Name"><b>Name</b></label>
+    <input type="text" placeholder="Enter name" name="Name" required>
     
-    #Getting specific value.
-    print("Specific Value")
-    Result = conn.execute(text("select Schedule_Id from CLASS where name = 'Calculus 3'"))
-    my_num = Result.first()[0]
-    print(my_num)
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
+""")
+
+def get_Tutor(name):
+    with engine.connect() as conn:
+        query=text(f"""Select * from Tutor where name = {name}""")
+        Result = conn.execute((query))
+        return Result.first()
+    
+def get_all_Tutors():
+    with engine.connect() as conn:
+        query=text(f"""Select name from Tutor""")
+        Result = conn.execute((query))
+        return Result.fetchall()
+
+def get_all_Tutors_by_class(class_id):
+    with engine.connect() as conn:
+        query=text(f"""SELECT Tutor.name from Tutor, Tutors_for where Tutor.tutor_num = Tutors_for.Tutor_num AND Tutors_for.Class_id = {class_id}""")
+        Result = conn.execute((query))
+        return Result.fetchall()
+    
+def get_all_classCodes():
+    with engine.connect() as conn:
+        query=text(f"""SELECT Class_id from CLASS""")
+        Result = conn.execute((query))
+        return Result.fetchall()
+    
+def make_appointment(mykey,mydate, mytime, tutor_id, tutee_id, Class_id):
+    with engine.connect() as conn:
+        mystring = f"INSERT INTO APPOINTMENT(Appointment_ID, a_date, a_time, Tutor_id, Tutee_id, Class_id) VALUES('{mykey}','{mydate}', '{mytime}', '{tutor_id}', '{tutee_id}', '{Class_id}')"
+        query = text(mystring)
+        conn.execute(query)
+        conn.commit()
+        return
+    
+def get_all_appointments():
+    with engine.connect() as conn:
+        query=text(f"""SELECT * from APPOINTMENT""")
+        Result = conn.execute((query))
+        return Result.fetchall()
+    
+def get_appointment(a_id):
+    with engine.connect() as conn:
+        query=text(f"""SELECT * from APPOINTMENT where Appointment_ID = {a_id}""")
+        Result = conn.execute((query))
+        return Result.fetchall()
+    
+
+print(get_all_appointments())
+make_appointment('1','1-28-2018', '13:30', 'A69420112', 'A5879321', '2156')
+print(get_all_appointments())
+print(get_all_Tutors())
+print(get_all_Tutors_by_class('1113'))
+print(get_all_classCodes())
 
 
-conn.commit()
 
+
+""" Working code. Use me if shit breaks:
+
+#For Testing
+    -- Was right below the appointment table --
+    mydate = '1-28-2018'
+    mytime = '13:00'
+    tutor_id = 'A69420112'
+    tutee_id = 'A5879321'
+    Class_id = '2156'
+    mystring = f"INSERT INTO APPOINTMENT(a_date, a_time, Tutor_id, Tutee_id, Class_id) VALUES('{mydate}', '{mytime}', '{tutor_id}', '{tutee_id}', '{Class_id}')"
+    query = text(mystring)
+    conn.execute(query)
+    #conn.execute(text("INSERT INTO APPOINTMENT(a_date, a_time, Tutor_id, Tutee_id, Class_id) VALUES('1-28-2018','13:30', 'A69420112','A5879321', '2156')"))
+    #No other prefill. Filled at runtime."""
