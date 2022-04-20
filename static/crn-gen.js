b6a30e31c5021c4f4eba398131c1aec65597fb61
keholsa <99051518+keholsa@users.noreply.github.com>
2022-04-20 18:15:42 -0500

    Create crn-gen.js

---
+++ static/crn-gen.js
@@ -0,0 +1,19 @@
+function crnAdd(){
+
+    var crnArrRaw = document.getElementById('crn-array-FLASK').innerHTML;
+    var selectBox = document.getElementById('crn-list');
+
+    crnArr = crnArrRaw.split(" ");
+
+
+    for(var i = 0; i < 5; i++){
+        var option = document.createElement('option');
+        selectBox.appendChild(option);
+        option.id = crnArr[i];
+        option.value = crnArr[i];
+        option.text = crnArr[i];
+
+    }
+    }
+
+    crnAdd();
