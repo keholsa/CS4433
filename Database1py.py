f87e7d80ba50fa5aaf9fb4e52eb8e324b1aad2b7
GipsonL <78498165+GipsonL@users.noreply.github.com>
2022-04-20 20:36:29 -0500

    Add files via upload

--- Database1py.py
+++ Database1py.py
@@ -12,61 +12,56 @@ session = sessionmaker(bind=engine)
 with engine.connect() as conn:
     conn.execute(text("""
 CREATE TABLE W_SCHEDULE(
-schedule_id INT auto_increment PRIMARY KEY,  
+schedule_id INT PRIMARY KEY,  
 Recurring INT,
-Monday_schedule text,
+Monday_schedule varchar(40),
 Tuesday_schedule varchar(40),
 Wednesday_schedule varchar(40),
 Thursday_schedule varchar(40),
 Friday_schedule varchar(40),
-Sunday_schedule varchar(40)
+Saturday_schedule varchar(40)
 )
 """))
     #Fill table
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','NA','14:00, 15:00','NA','10:00, 11:00', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','NA','12:00, 15:00','NA','10:00', '12:00, 13:00, 15:00')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'16:00, 17:00','NA','NA','NA','12:30, 13:30 PM', 'NA')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'10:00, 16:00 PM','10:00 AM, 3:00 PM','10:00 AM, 4:00 PM','10:00 AM, 3:00 PM','10:00 PM', '12:00 PM, 1:00 PM, 3:00 PM')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'16:00, 18:00, 20:00','NA','NA','NA','NA', '15:00, 12:00, 5:00 PM, 6:00 PM, 7:00 PM') """ ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'12:30 PM, 1:30 PM, 2:30 PM, 3:30 PM, 4:30 PM, 5:30 PM','NA','12:30 PM, 1:30 PM, 2:30 PM, 3:30 PM, 4:30 PM, 5:30 PM','NA','NA', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','NA','3:30 PM, 4:30 PM','NA','2:30 PM', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'1:00 PM, 2:00 PM','1:00 PM, 2:00 PM, 3:00 PM','NA','NA','NA','NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'10:30 AM, 11:30 AM, 12:30 PM, 1:30 PM, 2:30 PM','NA','8:00 AM','NA','NA','NA')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'10:30 AM, 11:30 AM, 12:30 PM, 1:30 PM, 2:30 PM','NA','8:00 AM','NA','NA','NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (1,1,'NA','NA','14:00, 15:00','NA','10:00, 11:00', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (2,1,'NA','NA','12:00, 15:00','NA','10:00', '12:00, 13:00, 15:00')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (3,1,'16:00, 17:00','NA','NA','NA','12:30, 13:30', 'NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (4,1,'10:00, 16:00','10:00, 12:00','10:00, 16:00','10:00, 15:00 PM','10:00', '12:00, 13:00, 15:00')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (5,1,'16:00, 18:00, 20:00','NA','NA','NA','NA', '15:00, 12:00, 5:00 PM, 6:00 PM, 7:00 PM') """ ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (6,1,'12:30, 13:30, 14:30, 15:30, 16:30, 17:30','NA','12:30, 13:30, 14:30, 15:30, 16:30, 17:30','NA','NA', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (7,1,'NA','NA','15:30, 16:30','NA','14:30', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (8,1,'13:00, 14:00','13:00, 14:00, 15:00','NA','NA','NA','NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (9,1,'10:30, 11:30, 12:30, 11:30, 14:30','NA','8:00','NA','NA','NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE 
+VALUES (10,1,'10:30, 11:30, 12:30, 11:30, 14:30','NA','8:00','NA','NA','NA')"""))
     
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'9:30 AM','NA','9:30 AM','NA','9:30 AM', 'NA')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','2:30 PM - 3:45','NA','2:30 PM - 3:45','NA', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'10:30','NA','10:30','NA','10:30', 'NA')""" ))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'4:00 PM - 5:15 PM','NA','4:00 PM - 5:15 PM','NA','NA', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'NA','2:30 PM - 3:45','NA','2:30 PM - 3:45','NA', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'9:30 AM','NA','9:30 AM','NA','9:30 AM', 'NA')"""))
-    conn.execute(text("""INSERT INTO W_SCHEDULE(Recurring, Monday_schedule, Tuesday_schedule, Wednesday_schedule, Thursday_schedule, Friday_schedule, Sunday_schedule) 
-VALUES (1,'4:00 PM - 5:15 PM','NA','4:00 PM - 5:15 PM','NA','NA', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (11,1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (12,1,'9:30 AM','NA','9:30 AM','NA','9:30 AM', 'NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (13,1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (14,1,'NA','2:30 PM - 3:45','NA','2:30 PM - 3:45','NA', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (15,1,'10:30','NA','10:30','NA','10:30', 'NA')""" ))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (16,1,'4:00 PM - 5:15 PM','NA','4:00 PM - 5:15 PM','NA','NA', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (17,1,'11:30 AM','NA','11:30 AM','NA','11:30 AM', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (18,1,'NA','2:30 PM - 3:45','NA','2:30 PM - 3:45','NA', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (19,1,'9:30 AM','NA','9:30 AM','NA','9:30 AM', 'NA')"""))
+    conn.execute(text("""INSERT INTO W_SCHEDULE VALUES (20,1,'4:00 PM - 5:15 PM','NA','4:00 PM - 5:15 PM','NA','NA', 'NA')"""))
     
+#Create copy for next week
+    conn.execute(text("""create table Schedule_2 as select * from W_SCHEDULE"""))
+#Create next week 2
+    conn.execute(text("""create table Schedule_3 as select * from W_SCHEDULE """))
+#Create table for the next week
+    conn.execute(text("""create table Schedule_4 as select * from W_SCHEDULE """))
     
-    
+    print(conn.execute(text("""select schedule_id from W_SCHEDULE """)).fetchall())
 
 #Tutee table
     conn.execute(text("""
@@ -130,13 +125,7 @@ Foreign Key (Schedule_Id) references W_SCHEDULE(schedule_id)
     conn.execute(text("INSERT INTO Tutor VALUES ('A69420110', 'Katie Williams', '806-867-5307', 8, 8)"))
     conn.execute(text("INSERT INTO Tutor VALUES ('A69420111', 'Elizabeth Carlson', '806-867-5308', 9, 9)"))
     conn.execute(text("INSERT INTO Tutor VALUES ('A69420112', 'Natali Edwards', '806-867-5309', 10, 10)"))
-    print()
-    print("MMMMMOREEE RESULTS LOOKIE HERE VVVVVVVVVV")
-    result = conn.execute(text("SELECT name from Tutor"))
-    for row in result:
-        print(f"{row.name}")
-    print("WEOOOOOWWWWEE RESULTS LOOKIE HERE" )
-    print()
+
     
 #Tutors for table
     #Create Table
@@ -164,24 +153,30 @@ Class_id varchar(8)
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (4, '1713')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (4, '1493')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (4, '1113')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (5, '1113')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (5, '3054')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (6, '1117')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (6, '1113')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (7, '2156')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (7, '2144')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (7, '1113')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (8, '3323')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (8, '1117')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (8, '1113')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (9, '1114')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (9, '1113')"))
     
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (10, '2156')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (10, '2144')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (10, '1493')"))
     conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (10, '1117')"))
+    conn.execute(text("INSERT INTO Tutors_for(Tutor_num, Class_id) VALUES (10, '1113')"))
         
     
 #Appointment table    
@@ -215,38 +210,6 @@ def hello_world():
     name = get_Tutor("'Mitchel Murray'")
     return f"{escape(name)}"
 
-@app.route("/SignUp")
-def sign_up():
-    #Oh yeah. I'm that guy. On server html. 
-    return ("""<form action="action_page.php" style="border:1px solid #ccc">
-  <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
-
-    <label for="email"><b>cwid</b></label>
-    <input type="text" placeholder="Enter cwid" name="cwid" required>
-    
-    <label for="Name"><b>Name</b></label>
-    <input type="text" placeholder="Enter name" name="Name" required>
-    
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-    <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
-    </div>
-  </div>
-</form>
-""")
 
 def get_Tutor(name):
     with engine.connect() as conn:
@@ -271,14 +234,17 @@ def get_all_classCodes():
         query=text(f"""SELECT Class_id from CLASS""")
         Result = conn.execute((query))
         return Result.fetchall()
-    
+
+occupied_appointments = []
 def make_appointment(mykey,mydate, mytime, tutor_id, tutee_id, Class_id):
-    with engine.connect() as conn:
-        mystring = f"INSERT INTO APPOINTMENT(Appointment_ID, a_date, a_time, Tutor_id, Tutee_id, Class_id) VALUES('{mykey}','{mydate}', '{mytime}', '{tutor_id}', '{tutee_id}', '{Class_id}')"
-        query = text(mystring)
-        conn.execute(query)
-        conn.commit()
-        return
+    if ((mydate, mytime, tutor_id, tutee_id, Class_id)) not in occupied_appointments:
+        with engine.connect() as conn:
+            mystring = f"INSERT INTO APPOINTMENT(Appointment_ID, a_date, a_time, Tutor_id, Tutee_id, Class_id) VALUES('{mykey}','{mydate}', '{mytime}', '{tutor_id}', '{tutee_id}', '{Class_id}')"
+            query = text(mystring)
+            conn.execute(query)
+            occupied_appointments.append((mydate, mytime, tutor_id, tutee_id, Class_id))
+            conn.commit()
+    return
     
 def get_all_appointments():
     with engine.connect() as conn:
@@ -299,8 +265,37 @@ def drop_appointment(a_id):
         conn.execute(query)
         conn.commit()
         return
+
+
     
-    
+def get_appointment_by_day(x, class_id):
+    with engine.connect() as conn:
+        days = ['Monday_schedule', 'Tuesday_schedule', 'Wednesday_schedule', 'Thursday_schedule', 'Friday_schedule',"Sunday",'Saturday_schedule']
+        day_of_week = days[x % 7]
+        if day_of_week == "Sunday":
+            return "Sorry, we're not open on Sunday"
+        if x < 8:
+            mystring = f"""SELECT {day_of_week} FROM W_SCHEDULE where {day_of_week} != 'NA' and
+                            schedule_id in (SELECT Tutor.Schedule_id from Tutor, Tutors_for where Tutor.tutor_num = Tutors_for.Tutor_num AND Tutors_for.Class_id = {class_id})"""
+            query = text(mystring)
+            return conn.execute(query).fetchall()
+        elif x < 15:
+            mystring = f"""SELECT {day_of_week} FROM SCHEDULE_2 where {day_of_week} != 'NA' and
+                            schedule_id in (SELECT Tutor.Schedule_id from Tutor, Tutors_for where Tutor.tutor_num = Tutors_for.Tutor_num AND Tutors_for.Class_id = {class_id})"""
+            query = text(mystring)
+            return conn.execute(query).fetchall()
+        elif x < 22:
+            mystring = f"""SELECT {day_of_week} FROM SCHEDULE_3 where {day_of_week} != 'NA' and
+                            schedule_id in (SELECT Tutor.Schedule_id from Tutor, Tutors_for where Tutor.tutor_num = Tutors_for.Tutor_num AND Tutors_for.Class_id = {class_id})"""
+            query = text(mystring)
+            return conn.execute(query).fetchall()
+        else:
+            mystring = f"""SELECT {day_of_week} FROM SCHEDULE_4 where {day_of_week} != 'NA' and
+                            schedule_id in (SELECT Tutor.Schedule_id from Tutor, Tutors_for where Tutor.tutor_num = Tutors_for.Tutor_num AND Tutors_for.Class_id = {class_id})"""
+            query = text(mystring)
+            return conn.execute(query).fetchall()
+            
+           
 
 print(get_all_appointments())
 make_appointment('1','1-28-2018', '13:30', 'A69420112', 'A5879321', '2156')
@@ -310,6 +305,8 @@ print(get_all_appointments())
 print(get_all_Tutors())
 print(get_all_Tutors_by_class('1113'))
 print(get_all_classCodes())
+print(get_all_Tutors_by_class('2156'))
+print(get_appointment_by_day(20, '1113'))
 
 
 
