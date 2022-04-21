d6693ed76548ba5d5e2ec9dd2771366aacb33e4c
keholsa <99051518+keholsa@users.noreply.github.com>
2022-04-20 22:48:00 -0500

    Update time-table-gen.js

--- static/time-table-gen.js
+++ static/time-table-gen.js
@@ -1,32 +1,45 @@
 function timeTable(){
 
+    //find container for time table
     var tableDiv = document.getElementById('select-time-container');
 
+    //create table in container
     var table = document.createElement('table');
-
     tableDiv.appendChild(table);
 
-    table.setAttribute("id", "select-time-container");
+    //set id attribute for 
+    table.setAttribute("id", "select-time-table");
 
+    //create table body
     var tableBody = document.createElement('tbody');
-
     table.appendChild(tableBody);
 
-    var rowIndex = 4
+    //variable grab for available times
+    var timeArrRaw = document.getElementById("time-array-FLASK").innerHTML;
+    timeArr = timeArrRaw.split(" ");
 
-    for(var i = 0; i < rowIndex; i++){
+    //definining row index
+    var rowIndex = timeArr.length
+
+    //dynamic row creation
+    for(var i = 0; i < rowIndex + 1; ++i){
 
         var tr = document.createElement('tr');
         tableBody.appendChild(tr);
         tr.setAttribute("id", "time-row-" + i);
 
+            //dynamic data creation per row
             for(var j = 0; j < 1; j++){
                 var td = document.createElement('td');
+                td.innerHTML = timeArr[i -1];
                 tr.appendChild(td);
                 td.setAttribute("id", "time-id-" + i);
+                td.setAttribute("tag", timeArr[i - 1]);
+                td.setAttribute("class", "time-td");
             }
     }
 
+    //initilize row definitition
     document.getElementById("time-id-0").innerHTML = "Times Available";
 }
 
